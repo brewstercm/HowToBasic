2429a7c5561d7f3bfea241173c3b19fc7f10c5ae
brewstercm <90487199+brewstercm@users.noreply.github.com>
2022-04-11 11:52:17 -0500

    Add files via upload

---
+++ HowToBasic/Categories.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Categories.aspx.cs" Inherits="HowToBasic.Categories" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Manage Categories</h2>
+    <asp:Panel ID="pnlAddCategories" runat="server">
+        <div class="row">
+            <div class="col-sm-2">
+                <label for="txtCategoryName">Category Name: </label>
+            </div>
+            <div class="col-sm-10">
+                <asp:TextBox ID="txtCategoryName" runat="server" placeholder="Please enter a name"></asp:TextBox>
+                <asp:Label ID="lblFeedback" runat="server" ForeColor="Black" Visible="False"></asp:Label>
+                <asp:RequiredFieldValidator ID="rfvCategoryName" runat="server" ControlToValidate="txtCategoryName" ErrorMessage="Required" ForeColor="#CC0000" ValidationGroup="AddCategory"></asp:RequiredFieldValidator>
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-sm-12">
+                <asp:Button ID="btnAddCategory" runat="server" Text="Add" CssClass="btn btn-primary" OnClick="btnAddCategory_Click" ValidationGroup="AddCategory" />
+                <asp:Button ID="btnSaveCategory" runat="server" CssClass="btn btn-primary" Text="Save" Visible="False" OnClick="btnSaveCategory_Click" />
+                <asp:Button ID="btnCancel" runat="server" CssClass="btn btn-secondary" Text="Cancel" Visible="False" OnClick="btnCancel_Click" />
+                <asp:Label ID="lblCategoryId" runat="server" Visible="False"></asp:Label>
+            </div>
+        </div>
+    </asp:Panel>
+    <br />
+    <asp:Panel ID="pnlCategoryList" runat="server">
+        <asp:GridView ID="gvCategoryList" runat="server" CssClass="table table-striped" AutoGenerateColumns="False" OnRowCommand="gvCategoryList_RowCommand" OnRowDataBound="gvCategoryList_RowDataBound">
+            <Columns>
+                <asp:BoundField DataField="CategoryID" HeaderText="ID" />
+                <asp:BoundField DataField="CategoryName" HeaderText="Category Name" />
+                <asp:TemplateField HeaderText="Actions">
+                    <ItemTemplate>
+                        <asp:Button ID="btnEdit" runat="server" CssClass="btn btn-primary" Text="Edit" CommandName="EditCategory" />
+                        <asp:Button ID="btnDelete" runat="server" CssClass="btn btn-danger"
+                            OnClientClick="return confirm('Are you sure you want to delete this category?')" Text="Delete" CommandName="DeleteCategory" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+
+</asp:Content>
---
+++ HowToBasic/Categories.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+
+namespace HowToBasic
+{
+    public partial class Categories : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindCategoryList();
+            }
+        }
+
+        protected void btnAddCategory_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                //1. Create a SqlConnection object
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+
+                //2. Create a SqlCommand object
+                SqlCommand cmd = new SqlCommand();
+
+                /// TODO:
+                /// We need to change the dynamic SQL statement later to avoid sql-injection attacks
+                cmd.CommandText = "INSERT INTO Categories VALUES ('" + txtCategoryName.Text + "')";
+                cmd.Connection = conn; //link the command to the connection object
+
+                //3. Open the connection
+                conn.Open();
+
+                //4. Execute the command
+                cmd.ExecuteNonQuery(); //this will insert data into the database
+
+                //5. Close the connection
+                conn.Close();
+
+                lblFeedback.Visible = true;
+                lblFeedback.Text = "The category <strong>" + txtCategoryName.Text + "</strong> was added successfully.";
+
+                BindCategoryList();
+            }
+        }
+
+        protected void BindCategoryList()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Categories";
+            cmd.Connection = conn;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            sda.Fill(dt);
+
+            gvCategoryList.DataSource = dt;
+            gvCategoryList.DataBind();
+            conn.Close();
+        }
+
+        protected void gvCategoryList_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Button editButton = e.Row.FindControl("btnEdit") as Button;
+                Button deleteButton = e.Row.FindControl("btnDelete") as Button;
+
+                editButton.CommandArgument = e.Row.Cells[0].Text;
+                deleteButton.CommandArgument = e.Row.Cells[0].Text;
+            }
+        }
+
+        protected void gvCategoryList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "EditCategory")//The edit button was clicked
+            {
+                int categoryId = int.Parse(e.CommandArgument.ToString());
+
+                EditCategoryById(categoryId);
+                lblCategoryId.Text = e.CommandArgument.ToString();
+
+            }
+            else if (e.CommandName == "DeleteCategory")//The delete button was clicked
+            {
+                int categoryId = int.Parse(e.CommandArgument.ToString());
+
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "DELETE FROM Categories WHERE CategoryID = " + categoryId;
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+                    BindCategoryList();
+                }
+                    
+            }
+        }
+
+        private void EditCategoryById(int categoryId)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT * FROM Categories WHERE CategoryID = " + categoryId;
+                cmd.Connection = conn;
+                conn.Open();
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                if (sdr.Read())
+                {
+                    txtCategoryName.Text = sdr["CategoryName"].ToString();
+                }
+
+                btnAddCategory.Visible = false;
+                btnSaveCategory.Visible = true;
+                btnCancel.Visible = true;
+                pnlCategoryList.Visible = false;
+                lblFeedback.Visible = false;
+            }
+           
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            btnAddCategory.Visible = true;
+            btnSaveCategory.Visible = false;
+            btnCancel.Visible=false;
+            pnlCategoryList.Visible = true;
+            txtCategoryName.Text = "";
+        }
+
+        protected void btnSaveCategory_Click(object sender, EventArgs e)
+        {
+            int categoryId = int.Parse(lblCategoryId.Text);
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+                SqlCommand cmd = new SqlCommand();
+                ///TODO 
+                ///we need to change the following statement to avoid
+                ///sql injection attacks
+                cmd.CommandText = "UPDATE Categories SET CategoryName='" + txtCategoryName.Text + "' WHERE CategoryID = " + categoryId;
+                cmd.Connection = conn;
+                conn.Open();
+
+                cmd.ExecuteNonQuery();
+
+                BindCategoryList();
+
+                btnAddCategory.Visible = true;
+                btnSaveCategory.Visible = false;
+                btnCancel.Visible = false;
+                pnlCategoryList.Visible = true;
+                txtCategoryName.Text = "";
+            }
+                
+        }
+    }
+}
