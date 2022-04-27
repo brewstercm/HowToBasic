e49c470ad14f58b2143caec699c5fa0b27c9ba05
Caleb Wiseman <wisemanca@jacks.sfasu.edu>
2022-04-27 12:21:48 -0500

    -made sandwich display from the database instead of being hardcoded -logo in navbar will now direct to default

--- HowToBasic/Sandwich.aspx.cs
+++ HowToBasic/Sandwich.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +13,33 @@ namespace HowToBasic
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (!Page.IsPostBack)
+            {
+                BindCategoryDDL();
+            }
+        }
+
+        private void BindCategoryDDL()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["HTBConnectionString"].ConnectionString;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT * From Tutorials WHERE TutorialTitle LIKE '%Sandwich%'";
+
+                cmd.Connection = conn;
+                conn.Open();
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                if (sdr.Read())
+                {
+                    sandwichImg.ImageUrl = sdr["TutorialThumbnail"].ToString();
+                    //display the actual tutorial here from the data base
+                    //sdr["TutorialSteps"].ToString();
+                }
+                
+            }
         }
     }
 }
